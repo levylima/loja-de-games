bff0db56fceb104424b3db81ffa4c6e071069789
levylima <levylima1510@gmail.com>
2022-10-17 21:43:29 -0300

    Alterações no main

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   process.env.TZ = '-03:00'
-//O pacote ValidationPipe ele está sendo habilitado na aplicação para que todas as requisições HTTP para checar se as requisições batem com a classe associada.
-  app.useGlobalPipes(new ValidationPipe())
+
+//O pacote ValidationPipe está sendo habilitado em todas as Requisições HTTP, ou seja, o Validation checará em todas as Requisições, cuja a Classe Entidade associada possua validações implementadas nos Atributos, no momento em que os dados forem enviados no Corpo de uma Requisição.
+
+  app.useGlobalPipes(new ValidationPipe());
+
+//O "enableCors está dizendo que aceita requisições de qualquer domínio."
+  app.enableCors();
 
   await app.listen(4000);
 }
